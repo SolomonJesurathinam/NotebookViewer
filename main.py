ed17f4cee616c87bbacd914e8361ec7aed64a978
Solomon <solomon258@gmail.com>
2023-11-20 16:18:11 +0530

    Html display

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import streamlit as st
 from nbconvert.nbconvertapp import main as nbmain
 import pdfkit
 import os
-import base64
 from pathlib import Path
+from streamlit_javascript import st_javascript
+from streamlit_lottie import st_lottie
+import json
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 
-st.set_page_config(page_title="NB to Pdf Converter")
+st.set_page_config(page_title="NB to PDF and HTML Converter")
 st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 
 with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()),unsafe_allow_html=True)
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
 def convert_notebook_to_html(notebook_path):
     nbmain(['--to', 'html', '--template', 'classic', notebook_path])
 
-st.title("Jupyter NB to Pdf Converter")
+# Load your Lottie animation file
+lottiepath = "loading.json"
+with open(lottiepath,"r") as file:
+    lottie_animation = json.load(file)
+
+st.title("Jupyter NB to PDF and HTML Converter")
 
 uploaded_file = st.file_uploader("Upload a Jupyter Notebook", type="ipynb")
 
@@ -34,30 +41,41 @@ if uploaded_file is not None:
     progress_bar = st.progress(0)
     status_message = st.empty()
 
-    # Convert Notebook to HTML
-    status_message.text("Converting notebook to HTML...")
-    progress_bar.progress(25)
-    convert_notebook_to_html(notebook_path)
+    # Display Lottie animation during conversion
+    lottie_container = st.empty()
+    with lottie_container:
+        st_lottie(lottie_animation, height=300, key="loading")
 
-    # Convert HTML to PDF
-    status_message.text("Converting HTML to PDF...")
-    progress_bar.progress(50)
-    try:
-        pdfkit.from_file(html_path, pdf_name, options={"enable-local-file-access": ""},verbose=True)
-    except OSError as e:
-        if 'Done' not in str(e):
-            st.error("Error while converting, please try another file")
+    if not os.path.exists(html_path) or not os.path.exists(pdf_name):
+        if not os.path.exists(html_path):
+            # Convert Notebook to HTML
+            status_message.text("Converting notebook to HTML...")
+            progress_bar.progress(25)
+            convert_notebook_to_html(notebook_path)
 
-    # Check if the PDF file was created
-    status_message.text("Finalizing...")
+        if not os.path.exists(pdf_name):
+            # Convert HTML to PDF
+            status_message.text("Converting HTML to PDF...")
+            progress_bar.progress(50)
+            try:
+                pdfkit.from_file(html_path, pdf_name, options={"enable-local-file-access": ""},verbose=True)
+            except OSError as e:
+                if 'Done' not in str(e):
+                    st.error("Error while converting, please try another file or reload and try again")
+
+    # Display HTML File and offer PDF for download
     progress_bar.progress(75)
-    if os.path.exists(pdf_name):
+    if os.path.exists(html_path) and os.path.exists(pdf_name):
+        # Hide the Lottie animation
+        lottie_container.empty()
+
+        ui_width = st_javascript("window.innerWidth")
+        with open(html_path, "r") as f:
+            html_content = f.read()
+            st.components.v1.html(html_content, width=ui_width, height=(ui_width*4/3), scrolling=True)
+
         with open(pdf_name, "rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            status_message.text("Preparing download...")
-            progress_bar.progress(100)
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="100%" style="height:80vh;"></iframe>'
-            st.markdown(pdf_display, unsafe_allow_html=True)
-            PDFByte = f.read()
+            pdf_data = f.read()
+            st.download_button(label="Download PDF", data=pdf_data, file_name=pdf_name, mime='application/pdf')
             status_message.text("Conversion complete! PDF is ready to download.")
-            st.download_button(label="Download PDF", data=f, file_name=pdf_name, mime='application/pdf')
+        progress_bar.progress(100)
