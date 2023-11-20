9c7e736a65bb49520c5ad10f07b66eb302655216
Solomon <solomon258@gmail.com>
2023-11-20 21:28:52 +0530

    updated code

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from pathlib import Path
 from streamlit_javascript import st_javascript
 from streamlit_lottie import st_lottie
 import json
-
 import shutil
+
+# 0.7249087159990619
+# 0.7249205380285673
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
@@ -18,19 +20,34 @@ st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allo
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-def get_disk_usage(path):
-    """Returns the disk usage of the specified path."""
-    total, used, free = shutil.disk_usage(path)
-    return used / total
+def clear_storage_if_needed(storage_dir, threshold=0.7249505380285673):
+    # Get disk usage statistics
+    total, used, free = shutil.disk_usage(storage_dir)
 
-st.write(get_disk_usage(current_dir))
+    # Calculate the percentage of space used
+    percent_used = used / total
+
+    st.write(percent_used)
+
+    # If used space exceeds the threshold, delete files
+    if percent_used > threshold:
+        for file in Path(storage_dir).iterdir():
+            try:
+                if file.is_file():
+                    os.remove(file)
+                    print(f"Deleted {file}")
+            except Exception as e:
+                print(f"Error deleting {file}: {e}")
+
+clear_storage_if_needed(current_dir /"files/")
 
 def convert_notebook_to_html(notebook_path):
     nbmain(['--to', 'html', '--template', 'classic', notebook_path])
 
+
 # Load your Lottie animation file
 lottiepath = "loading.json"
-with open(lottiepath,"r") as file:
+with open(lottiepath, "r") as file:
     lottie_animation = json.load(file)
 
 st.title("Jupyter NB to PDF and HTML Converter")
@@ -39,9 +56,9 @@ uploaded_file = st.file_uploader("Upload a Jupyter Notebook", type="ipynb")
 
 if uploaded_file is not None:
     file_name = uploaded_file.name
-    notebook_path = file_name
-    html_path = file_name.replace(".ipynb", ".html")
-    pdf_name = file_name.replace(".ipynb", ".pdf")
+    notebook_path = "files/" + file_name
+    html_path = "files/" + file_name.replace(".ipynb", ".html")
+    pdf_name = "files/" + file_name.replace(".ipynb", ".pdf")
 
     # Save the uploaded notebook temporarily
     with open(notebook_path, "wb") as f:
@@ -67,7 +84,7 @@ if uploaded_file is not None:
             status_message.text("Converting HTML to PDF...")
             progress_bar.progress(50)
             try:
-                pdfkit.from_file(html_path, pdf_name, options={"enable-local-file-access": ""},verbose=True)
+                pdfkit.from_file(html_path, pdf_name, options={"enable-local-file-access": ""}, verbose=True)
             except OSError as e:
                 if 'Done' not in str(e):
                     st.error("Error while converting, please try another file or reload and try again")
@@ -81,7 +98,7 @@ if uploaded_file is not None:
         ui_width = st_javascript("window.innerWidth")
         with open(html_path, "r") as f:
             html_content = f.read()
-            st.components.v1.html(html_content, width=ui_width, height=(ui_width*4/3), scrolling=True)
+            st.components.v1.html(html_content, width=ui_width, height=(ui_width * 4 / 3), scrolling=True)
 
         with open(pdf_name, "rb") as f:
             pdf_data = f.read()
@@ -89,6 +106,6 @@ if uploaded_file is not None:
             status_message.text("Conversion complete! PDF is ready to download.")
         progress_bar.progress(100)
 
-    #os.remove(file_name)
-    #os.remove(html_path)
-    #os.remove(pdf_name)
+    # os.remove(file_name)
+    # os.remove(html_path)
+    # os.remove(pdf_name)
