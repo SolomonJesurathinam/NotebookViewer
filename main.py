aef0f0b33ecc3182cf1de3fc88643447e6818033
Solomon <solomon258@gmail.com>
2023-11-21 12:42:13 +0530

    Final commit - updated webpdf

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import shutil
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 
-st.set_page_config(page_title="NB to PDF and HTML Converter")
+st.set_page_config(page_title="NB to PDF and HTML Converter",layout='wide',page_icon=":books:")
 st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 
 with open(css_file) as f:
@@ -47,9 +47,9 @@ lottiepath = "loading.json"
 with open(lottiepath, "r") as file:
     lottie_animation = json.load(file)
 
-st.title("Jupyter NB to PDF and HTML Converter")
+st.title("Jupyter NB to PDF and HTML Converter :hammer:")
 
-uploaded_file = st.file_uploader("Upload a Jupyter Notebook", type="ipynb")
+uploaded_file = st.file_uploader("Upload a Jupyter Notebook :chart_with_upwards_trend:", type="ipynb")
 
 if uploaded_file is not None:
     file_name = uploaded_file.name
@@ -72,7 +72,7 @@ if uploaded_file is not None:
     if not os.path.exists(html_path) or not os.path.exists(pdf_name):
         if not os.path.exists(html_path):
             # Convert Notebook to HTML
-            status_message.text("Converting notebook to HTML...")
+            status_message.text("Converting notebook to HTML... 📖")
             progress_bar.progress(25)
             convert_notebook_to_html(notebook_path)
 
@@ -85,7 +85,7 @@ if uploaded_file is not None:
 
         if not os.path.exists(pdf_name):
             # Convert HTML to PDF
-            status_message.text("Converting Ipynb to PDF...")
+            status_message.text("Converting Ipynb to PDF... 📝")
             nbmain(['--to', 'webpdf', '--allow-chromium-download', notebook_path])
 
     # Display HTML File and offer PDF for download
@@ -95,14 +95,17 @@ if uploaded_file is not None:
         lottie_container.empty()
 
         ui_width = st_javascript("window.innerWidth")
+        # Calculate the height based on a 16:9 aspect ratio
+        aspect_ratio = 9 / 16
+        ui_height = ui_width / aspect_ratio
         with open(html_path, "r") as f:
             html_content = f.read()
-            st.components.v1.html(html_content, width=ui_width, height=(ui_width * 4 / 3), scrolling=True)
+            st.components.v1.html(html_content, width=ui_width, height=ui_height, scrolling=True)
 
         with open(pdf_name, "rb") as f:
             pdf_data = f.read()
             st.download_button(label="Download PDF", data=pdf_data, file_name=pdf_name, mime='application/octet-stream')
-            status_message.text("Conversion complete! PDF is ready to download.")
+            status_message.text("Conversion complete! PDF is ready to download.. 📜")
         progress_bar.progress(100)
 
     # os.remove(file_name)
